1875b77bcaa6529bc8ad0306130dba52aa060fb8
Marcelo Ortiz <ortizludwin68@outlook.com>
2025-03-17 12:36:17 -0700

    Configured

---
+++ src/App.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import svelteLogo from './assets/svelte.svg'
+  import viteLogo from '/vite.svg'
+  import Counter from './lib/Counter.svelte'
+</script>
+
+<main class="p-4">
+  <div class="flex items-center justify-center space-x-4">
+  </div>
+</main>
